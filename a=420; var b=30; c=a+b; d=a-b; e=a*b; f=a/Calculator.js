2a74e0fdf2c4b3c8ffc656ca4a8489f529d3c092
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-31 22:24:59 +0530

    Create Calculator.js

---
+++ a=420; var b=30; c=a+b; d=a-b; e=a*b; f=a/Calculator.js
@@ -0,0 +1,11 @@
+a=420;
+var b=30;
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+console.log("add=",c);
+console.log("sub=",d);
+console.log("mul=",e);
+console.log("divide=",f);
+console.log("wrote by Vishwa V")
