03ad4609fb124d71a3cc37cee2ce2582964bcaeb
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-11 19:17:15 +0530

    Create Split.js

---
+++ Split.js
@@ -0,0 +1,6 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Welcome to split program");
+var n="hello";
+console.log(n.split(""));
