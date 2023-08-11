113fa4c09ffa57cc94bc899353bca615582323c2
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-11 19:19:12 +0530

    Create Replace.js

---
+++ Replace.js
@@ -0,0 +1,6 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Welcome to Programiz!");
+var n="hello";
+console.log(n.replace("","yy"));
