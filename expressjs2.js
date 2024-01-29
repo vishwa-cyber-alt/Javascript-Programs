61947b29f5d7e38e722a6eec9e375ead3f74c7ea
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2024-01-29 19:29:19 +0530

    Create expressjs2.js

---
+++ expressjs2.js
@@ -0,0 +1,13 @@
+const vishwa='vishwa'
+const vishwabalance=2500
+const raj='raj'
+const rajbalance=2100
+const sayHello=(name)=>{
+console.log(`hello ${name} `)
+}
+console.log(`vishwa you have rs ${vishwabalance}`)
+console.log(`raj you have rs ${rajbalance}`)
+sayHello('rama')
+sayHello(raj)
+sayHello(vishwa)
+sayHello('rao')
